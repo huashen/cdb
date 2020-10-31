4b1ffdd5de3832796f65f7925d87a208f0c39e09
longhuashen <huashen361@163.com>
2020-10-31 20:31:55 +0800

    添加接收命令行参数逻辑

---
+++ README.md
@@ -0,0 +1,4 @@
+### 用c语言写的一个简易数据库，使用clion开发(文件部分仅支持unix*系统)
+参考：https://cstack.github.io/db_tutorial/parts/part1.html
+
+### 阶段一：制作一个简单的命令行执行程序
---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <memory.h>
+
+typedef struct {
+    char *buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+
+void print_prompt();
+
+InputBuffer *new_input_buffer();
+
+void read_input(InputBuffer *buffer);
+
+void close_input_buffer(InputBuffer *buffer);
+
+int main(int argc, char *argv[]) {
+    InputBuffer *input_buffer = new_input_buffer();
+    //循环接收命令行参数
+    while (true) {
+        print_prompt();
+        read_input(input_buffer);
+
+        if (strcmp(input_buffer->buffer, ".exit") == 0) {
+           close_input_buffer(input_buffer);
+           exit(EXIT_SUCCESS);
+        } else {
+            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        }
+    }
+}
+
+void close_input_buffer(InputBuffer *input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+void read_input(InputBuffer *input_buffer) {
+    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+    if (bytes_read <= 0) {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
+    }
+
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+InputBuffer *new_input_buffer() {
+    InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+
+    return input_buffer;
+}
+
+//命令提示
+void print_prompt() {
+    printf("db >");
+}
