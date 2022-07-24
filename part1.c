4ecf5289312efce40483da2311b9aefb406215d2
huashen <huashen361@gmail.com>
2022-07-25 00:23:01 +0800

    part1

--- part1.c
+++ part1.c
@@ -42,3 +42,39 @@ typedef struct {
     uint32_t buffer_length;
     uint32_t str_length;
 } InputBuffer;
+
+InputBuffer* new_input_buffer() {
+    InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->str_length = 0;
+    input_buffer->buffer_length = 0;
+    return input_buffer;
+}
+
+void del_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+//指明username 大小为32字节
+const uint32_t COLUMN_USERNAME = 32;
+
+//指明email 大小为255字节
+const uint32_t COLUMN_EMAIL = 255;
+
+//Row 代表写入的表类型结构 |<4>id|<33>(username)|<256>(email)|
+typedef struct {
+    uint32_t id;
+    // +1 是为了gei '\0' 保留一位
+    char username[COLUMN_USERNAME + 1];
+    char email[COLUMN_EMAIL + 1];
+} Row;
+
+// Statement 对象为操作对象
+typedef struct {
+  StatementType type;
+  Row row_to_insert;
+} Statement;
+
+#define FORLESS(less) for (int i =0; i < less; i++)
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
