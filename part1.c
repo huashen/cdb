ae6b2e99480e79cc6952623f348c08c17fd30009
huashen <huashen361@gmail.com>
2022-08-16 17:02:33 +0800

    db关闭保存数据到文件

--- part1.c
+++ part1.c
@@ -134,9 +134,35 @@ void read_line(InputBuffer* input_buffer) {
     input_buffer->str_length = strlen(input_buffer->buffer);
 }
 
-void db_close(Table* table) {
-//    Pager* pager = table->pager;
+//close & flush
+void pager_flush(Pager* pager, uint32_t page_num, uint32_t size) {
+    if (pager->pages[page_num] == NULL) {
+        printf("flush error by empty page at %d, size: %d .\n", page_num, size);
+        exit(EXIT_FAILURE);
+    }
+    off_t offset = lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+    if (offse == -1) {
+        printf("flush seek page at %d, error: %s .\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    ssize_t write_bytes = write(pager->file_descriptor, pager->pages[page_num], size);
+    if (write_bytes == -1) {
+        printf("flush write page at %d, error: %s.\n", page_num, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+}
 
+void db_close(Table* table) {
+   Pager* pager = table->pager;
+   //计算需要写入的整页面数
+   uint32_t full_num_rows = table->row_nums / ROW_PER_PAGES;
+   FORLESS(full_num_rows) {
+       if (pager->pages[i] != NULL) {
+           pager_flush(pager, i, PAGE_SIZE);
+           free(pager->pages[i]);
+           pager->pages[i] = NULL;
+       }
+   } 
 }
 
 MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
